1844c5660e133030e122ae8932b156f72cdc8816
Matt Gruskin <matthew.gruskin@gmail.com>
2016-02-21 19:20:25 -0500

    add morgan for request logging

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var config = require('config');
 var express = require('express');
+var morgan = require('morgan');
 
 var app = express();
+app.use(morgan('short'));
 app.use('/teams', require('./routes/teams'));
 
 app.listen(config.get('jscube.port'));
