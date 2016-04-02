6fa1710e413c079e2cfca2d3df8e00e386e2e95e
Matt Gruskin <matthew.gruskin@gmail.com>
2016-04-01 23:00:46 -0400

    validate FullRelease event submission

--- db/puzzles.js
+++ db/puzzles.js
@@ -1,5 +1,6 @@
 var async = require('async');
 var db = require('./db').db;
+var errorUtil = require('../util/error');
 
 function createPuzzles(puzzleIds, callback) {
   async.each(puzzleIds, (puzzleId, cb) => {
@@ -10,3 +11,14 @@ function createPuzzles(puzzleIds, callback) {
   }, callback);
 }
 module.exports.create = createPuzzles;
+
+function puzzleExists(puzzleId, callback) {
+  db.query(
+    'SELECT puzzleId FROM puzzles WHERE puzzleId = ?',
+    [puzzleId],
+    (err, result) => {
+      errorUtil.thrower(err);
+      callback(null, result.rows.length > 0);
+    });
+}
+module.exports.exists = puzzleExists;
--- routes/events.js
+++ routes/events.js
@@ -1,11 +1,31 @@
+var _ = require('underscore');
+var async = require('async');
+var dbPuzzles = require('../db/puzzles');
 var eventEmitter = require('../events/emitter');
 var express = require('express');
 
 var router = express.Router();
 
 router.post('/', (req, res) => {
-  eventEmitter.emit(req.body.eventType, req.body);
-  res.json(req.body);
+  var validators = [];
+
+  switch (req.body.eventType) {
+  case 'FullRelease':
+    validators.push(cb => dbPuzzles.exists(req.body.puzzleId, cb));
+    break;
+  }
+
+  async.parallel(validators, (err, results) => {
+    if (err) {
+      return res.status(400).send(err.message);
+    }
+    if (_.every(results)) {
+      eventEmitter.emit(req.body.eventType, req.body);
+      res.json(req.body);
+    } else {
+      return res.status(400).send('invalid event');
+    }
+  });
 });
 
 module.exports = router;
